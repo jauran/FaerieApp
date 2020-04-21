4c68ece277539b22109ba4e6d5592c4a53862e00
jauran <58847302+jauran@users.noreply.github.com>
2020-04-21 11:49:27 +0800

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -9,9 +9,9 @@ import java.util.*;
 
 public class MainActivity extends Activity {
 	//private TextView TextView = null;
-	private ImageView ImageView1=null;
-	private ImageView ImageView4=null;
-	int i,j=0;
+	private ImageView ImageView1 = null;
+	private ImageView ImageView4 = null;
+	int i, j = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,36 +23,36 @@ public class MainActivity extends Activity {
 	public void onClickImageView1(View view) {
 		i = i + 1;
 		String[] talk = getResources().getStringArray(R.array.talk);
-		Random random =new Random();
-		int t=random.nextInt(talk.length);
-		final int m=random.nextInt(360) - ImageView1.getLeft() + 80,n=random.nextInt(800) - ImageView1.getTop() + 560;
+		Random random = new Random();
+		int t = random.nextInt(talk.length);
+		final int m = random.nextInt(360) - ImageView1.getLeft() + 80, n = random.nextInt(800) - ImageView1.getTop() + 560;
 		// Animation...
-		TranslateAnimation animation = new TranslateAnimation(0, m, 0, n);//右移280
+		TranslateAnimation animation = new TranslateAnimation(0, m, 0, n);//从0原位，左右移动m，上下移动n
 		animation.setDuration(3600);
 		animation.setFillAfter(true);
-		animation.setAnimationListener(new Animation.AnimationListener(){
-				@Override
-				public void onAnimationStart(Animation animation) {
-				}
-				@Override
-				public void onAnimationRepeat(Animation animation) {
-				}
-				@Override
-				public void onAnimationEnd(Animation animation) {
-					ImageView1.clearAnimation();//动画结束时清除动画，并在下面给它位置
-					FrameLayout.LayoutParams layoutParams=(FrameLayout.LayoutParams)ImageView1.getLayoutParams();
-					//layoutParams.leftMargin = ImageView1.getLeft() + m;
-					layoutParams.setMargins(ImageView1.getLeft() + m, ImageView1.getTop() + n, 0, 0);
-					ImageView1.setLayoutParams(layoutParams);
-				}
-			});
+		animation.setAnimationListener(new Animation.AnimationListener() {
+			@Override
+			public void onAnimationStart(Animation animation) {
+			}
+			@Override
+			public void onAnimationRepeat(Animation animation) {
+			}
+			@Override
+			public void onAnimationEnd(Animation animation) {
+				ImageView1.clearAnimation();//动画结束时清除动画，并在下面给它位置
+				FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams)ImageView1.getLayoutParams();
+				//layoutParams.leftMargin = ImageView1.getLeft() + m;
+				layoutParams.setMargins(ImageView1.getLeft() + m, ImageView1.getTop() + n, 0, 0);
+				ImageView1.setLayoutParams(layoutParams);
+			}
+		});
 		view.startAnimation(animation);
 		if (i == 1) {
 			// This Java code shows a toast
 			Toast.makeText(this, "你好!\n我是赵然制作的第1个安卓小机器人^_^", Toast.LENGTH_LONG).show();
 		}
 		else if (i == 2) {
-			Toast.makeText(this, "送你招财进宝符，保佑你好财运", Toast.LENGTH_LONG).show();
+			Toast.makeText(this, "我这里有1张招财进宝符，保佑你好财运", Toast.LENGTH_LONG).show();
 			ImageView4.setVisibility(ImageView.VISIBLE);
 		}
 		else if (i == 3) {
@@ -83,11 +83,15 @@ public class MainActivity extends Activity {
 			// This Java code shows a toast
 			Toast.makeText(this, "我够到树上钥匙才能进入小屋!", Toast.LENGTH_LONG).show();
 		}
-		else if (j == 3) {
-			Toast.makeText(this, "这是第" + j + "次告诉你：\n我够到树上钥匙才能进入小屋!", Toast.LENGTH_LONG).show();
+		else if (j >= 3) {
+			Toast.makeText(this, "我还进不去小屋!", Toast.LENGTH_LONG).show();
 		}
 	}
 	public void onClickImageView4(View view) {
+		try {
+			Thread.sleep(1000);
+		}
+		catch (InterruptedException e) {}
 		view.setVisibility(View.INVISIBLE);
 	}
 }
